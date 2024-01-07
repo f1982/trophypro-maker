c1bdd9057cd1609f84822b919faecb26ba9b6c44
Andy <caojundan@gmail.com>
2024-01-07 17:15:32 +1300

    chore: opt code

--- app/3d/hanger/u-shape-slot.ts
+++ app/3d/hanger/u-shape-slot.ts
@@ -2,10 +2,9 @@ const { TAU } = require("@jscad/modeling").maths.constants;
 
 import { subtract } from "@jscad/modeling/src/operations/booleans";
 import { extrudeLinear } from "@jscad/modeling/src/operations/extrusions";
-import { cuboid, cylinder, polygon } from "@jscad/modeling/src/primitives";
-import { union } from "lodash";
-import { getScrewHole } from "./wing";
 import { rotate, translate } from "@jscad/modeling/src/operations/transforms";
+import { polygon } from "@jscad/modeling/src/primitives";
+import { getScrewHole } from "./wing";
 
 export const getUShapeSlot = (
   depth: number = 10,
@@ -17,39 +16,38 @@ export const getUShapeSlot = (
   const bumpHeight = 2;
   const slotDepth = height - 2.4;
 
-  const model = extrudeLinear(
-    { height: depth },
-    polygon({
-      points: [
-        [0, 0],
-        [lw, 0],
-        [lw, slotDepth],
-        [lw + slotGap, slotDepth],
-        [lw + slotGap, 0],
-        [lw + slotGap + rw, 0],
-        [lw + slotGap + rw, height],
-        // bump
-        [lw + slotGap, height + bumpHeight],
-        [lw, height + bumpHeight],
-        [lw - bumpHeight, height],
+  // Create base shape
+  const base = polygon({
+    points: [
+      [0, 0],
+      [lw, 0],
+      [lw, slotDepth],
+      [lw + slotGap, slotDepth],
+      [lw + slotGap, 0],
+      [lw + slotGap + rw, 0],
+      [lw + slotGap + rw, height],
+      [lw + slotGap, height + bumpHeight],
+      [lw, height + bumpHeight],
+      [lw - bumpHeight, height],
+      [0, height],
+    ],
+  });
 
-        //
-        [0, height],
-      ],
-    })
-  );
+  // Extrude base shape
+  const model = extrudeLinear({ height: depth }, base);
 
+  // Create screw holes
   const screwRadius = 1.4;
-
-  return subtract(
-    model,
-    translate(
-      [lw, height / 2, depth / 5],
-      rotate([0, -TAU / 4, 0], getScrewHole(lw, screwRadius))
-    ),
-    translate(
-      [lw, height / 2, (depth / 5) * (5 - 1)],
-      rotate([0, -TAU / 4, 0], getScrewHole(lw, screwRadius))
-    )
+  const screw1 = translate(
+    [lw, height / 2, depth / 5],
+    rotate([0, -TAU / 4, 0], getScrewHole(lw, screwRadius))
   );
+
+  const screw2 = translate(
+    [lw, height / 2, (depth / 5) * (5 - 1)],
+    rotate([0, -TAU / 4, 0], getScrewHole(lw, screwRadius))
+  );
+
+  // Subtract screw holes
+  return subtract(model, screw1, screw2);
 };
